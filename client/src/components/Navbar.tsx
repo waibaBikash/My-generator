7bb921506a57e34da0f09a9d4774e26f3c59e780
waibaBikash <kavrelibikash@gmail.com>
2026-02-02 22:54:02 +1100

    feat: update Navbar component with new routing links and improved navigation structure

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,10 +3,12 @@ import { useState } from "react";
 import { motion } from "motion/react";
 import { navlinks } from "../data/navlinks";
 import type { INavLink } from "../types";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
+
+    const navigate = useNavigate();
 
     return (
         <>
@@ -16,20 +18,28 @@ export default function Navbar() {
                 viewport={{ once: true }}
                 transition={{ type: "spring", stiffness: 250, damping: 70, mass: 1 }}
             >
-                <a href="https://prebuiltui.com?utm_source=pixels">
-                    <img className="h-8.5 w-auto" src="/assets/logo.svg" alt="logo" width={130} height={34} />
-                </a>
+                <Link to="/">
+                    <img src="/logo.svg" alt="logo" className="h-8.5 w-auto" />
+                </Link>
 
                 <div className="hidden md:flex items-center gap-8 transition duration-500">
-                    {navlinks.map((link: INavLink) => (
-                        <NavLink key={link.name} to={link.href} className="hover:text-pink-500 transition">
-                            {link.name}
-                        </NavLink>
-                    ))}
+                    <Link to="/" className=" hover:text-pink-300 transition">
+                    Home
+                    </Link>
+                    <Link to="/generate" className=" hover:text-pink-300 transition">
+                    Generator
+                    </Link>
+                    <Link to="/my-generation" className=" hover:text-pink-300 transition">
+                    My Generations
+                    </Link>
+                    <Link to="#" className=" hover:text-pink-300 transition">
+                    Contact
+                    </Link>
+                    
                 </div>
 
-                <button className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
-                    Start free trial
+                <button onClick={()=> navigate('/login')} className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
+                    Get Started
                 </button>
                 <button onClick={() => setIsOpen(true)} className="md:hidden">
                     <MenuIcon size={26} className="active:scale-90 transition" />
@@ -37,11 +47,19 @@ export default function Navbar() {
             </motion.nav>
 
             <div className={`fixed inset-0 z-100 bg-black/40 backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-400 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
-                {navlinks.map((link: INavLink) => (
-                    <NavLink key={link.name} to={link.href} onClick={() => setIsOpen(false)}>
-                        {link.name}
-                    </NavLink>
-                ))}
+                <Link onClick={() => setIsOpen(false)} to='/'>Home</Link>
+                 <Link  onClick={() => setIsOpen(false)}to="/generate">
+                    Generator
+                    </Link >
+                    <Link onClick={() => setIsOpen(false)} to="/my-generation">
+                    My Generations
+                    </Link>
+                    <Link to="#">
+                    My Contact
+                    </Link>
+                    <Link onClick={() => setIsOpen(false)} to="/login">
+                    Login
+                    </Link>
                 <button onClick={() => setIsOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-pink-600 hover:bg-pink-700 transition text-white rounded-md flex">
                     <XIcon />
                 </button>

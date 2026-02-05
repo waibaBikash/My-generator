2d07d9a390b8fca25bb863638627d4150daf765b
waibaBikash <kavrelibikash@gmail.com>
2026-02-05 22:35:39 +1100

    feat: update FeaturesSection with new section title and image rendering for features

--- client/src/sections/FeaturesSection.tsx
+++ client/src/sections/FeaturesSection.tsx
@@ -8,7 +8,7 @@ import type { IFeature } from "../types";
 export default function FeaturesSection() {
     return (
         <div id="features" className="px-4 md:px-16 lg:px-24 xl:px-32">
-            <SectionTitle text1="Features" text2="What you get" text3="Components, patterns and pages — everything you need to ship." />
+            <SectionTitle text1="Features" text2="Why use our generator?" text3="Create stunning thumbnails that get clicks, without the hassle." />
             <div className="flex flex-wrap items-center justify-center gap-6 md:gap-4 mt-16 px-6">
                 {featuresData.map((feature: IFeature, index: number) => (
                     <motion.div key={index} className={`${index === 1 ? 'p-px rounded-[13px] bg-linear-to-br from-pink-600 to-slate-800' : ''}`}
@@ -18,7 +18,8 @@ export default function FeaturesSection() {
                         transition={{ delay: index * 0.15, type: "spring", stiffness: 320, damping: 70, mass: 1 }}
                     >
                         <div className="p-6 rounded-xl space-y-4 border border-slate-800 bg-slate-950 max-w-80 w-full">
-                            {feature.icon}
+                            
+                            <img src={feature.icon} alt={feature.title} />
                             <h3 className="text-base font-medium text-white">
                                 {feature.title}
                             </h3>

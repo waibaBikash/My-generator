2a5791669a8f1f6fb723525afd46b622c50621ed
waibaBikash <kavrelibikash@gmail.com>
2026-02-09 22:50:02 +1100

    feat: update TestimonialSection content to enhance messaging and user engagement

--- client/src/sections/TestimonialSection.tsx
+++ client/src/sections/TestimonialSection.tsx
@@ -7,7 +7,7 @@ import Marquee from "react-fast-marquee";
 export default function TestimonialSection() {
     return (
         <div id="testimonials" className="px-4 md:px-16 lg:px-24 xl:px-32">
-            <SectionTitle text1="Testimonials" text2="Don't just take our words" text3="Hear what our users say about us. We're always looking for ways to improve. If you have a positive experience with us, leave a review." />
+            <SectionTitle text1="Testimonials" text2="Loved by creators" text3="See how our AI thumbnails are helping channels explode their views." />
 
             <Marquee className="max-w-5xl mx-auto mt-11" gradient={true} speed={25} gradientColor="#000">
                 <div className="flex items-center justify-center py-5 overflow-hidden">

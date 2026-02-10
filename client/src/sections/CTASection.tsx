9dd0d783c59234b38761cc6379841065ba71dec9
waibaBikash <kavrelibikash@gmail.com>
2026-02-10 21:59:29 +1100

    feat: update CTASection messaging and add navigation for enhanced user engagement

--- client/src/sections/CTASection.tsx
+++ client/src/sections/CTASection.tsx
@@ -1,7 +1,10 @@
 'use client'
 import { motion } from "motion/react";
+import { useNavigate } from "react-router-dom";
 
 export default function CTASection() {
+
+    const navigate = useNavigate()
     return (
         <motion.div className="max-w-5xl py-16 mt-40 md:pl-20 md:w-full max-md:mx-4 md:mx-auto flex flex-col md:flex-row max-md:gap-6 items-center justify-between text-left bg-linear-to-b from-pink-900 to-pink-950 rounded-2xl p-6 text-white"
             initial={{ y: 150, opacity: 0 }}
@@ -16,7 +19,7 @@ export default function CTASection() {
                     viewport={{ once: true }}
                     transition={{ type: "spring", stiffness: 280, damping: 70, mass: 1 }}
                 >
-                    Ready to try-out this app?
+                    Ready to go viral?
                 </motion.h1>
                 <motion.p className="bg-linear-to-r from-white to-pink-400 text-transparent bg-clip-text text-lg"
                     initial={{ y: 80, opacity: 0 }}
@@ -24,16 +27,16 @@ export default function CTASection() {
                     viewport={{ once: true }}
                     transition={{ type: "spring", stiffness: 200, damping: 70, mass: 1 }}
                 >
-                    Your next favourite tool is just one click away.
+                    Join Thousands of creators using AI to boast their CTR.
                 </motion.p>
             </div>
-            <motion.button className="px-12 py-3 text-slate-800 bg-white hover:bg-slate-200 rounded-full text-sm mt-4"
+            <motion.button onClick={()=> navigate('/generate')} className="px-12 py-3 text-slate-800 bg-white hover:bg-slate-200 rounded-full text-sm mt-4"
                 initial={{ y: 80, opacity: 0 }}
                 whileInView={{ y: 0, opacity: 1 }}
                 viewport={{ once: true }}
                 transition={{ type: "spring", stiffness: 280, damping: 70, mass: 1 }}
             >
-                Get Started
+                Generate Free Thumbnail
             </motion.button>
         </motion.div>
     );

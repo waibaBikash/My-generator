e80a2e3f6680d7ce42f3c2ddfb65933330e3b5ad
waibaBikash <kavrelibikash@gmail.com>
2026-02-10 22:04:06 +1100

    feat: update footer logo link and rename brand to Thumblyfy for consistency

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -13,9 +13,9 @@ export default function Footer() {
                 viewport={{ once: true }}
                 transition={{ type: "spring", stiffness: 280, damping: 70, mass: 1 }}
             >
-                <a href="https://prebuiltui.com?utm_source=pixels">
-                    <img className="size-8 aspect-square" src="/assets/footer-logo.svg" alt="footer logo" width={32} height={32} />
-                </a>
+                <Link to='/'>
+                    <img className="size-8 aspect-square" src="/favicon.svg" alt="footer logo" width={32} height={32} />
+                </Link>
                 {footerData.map((section, index) => (
                     <div key={index}>
                         <p className="text-slate-100 font-semibold">{section.title}</p>
@@ -52,7 +52,7 @@ export default function Footer() {
                         <YoutubeIcon className="size-6 hover:text-pink-500" />
                     </a>
                 </div>
-                <p className="mt-3 text-center">&copy; {new Date().getFullYear()} <a href="https://prebuiltui.com?utm_source=pixels">PrebuiltUI</a></p>
+                <p className="mt-3 text-center">&copy; {new Date().getFullYear()} <a href="https://prebuiltui.com?utm_source=pixels">Thumblyfy</a></p>
             </motion.div>
         </footer>
     );

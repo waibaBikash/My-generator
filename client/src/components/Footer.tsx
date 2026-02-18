e2e3586b4d5a95cfc21d098c181eaa29b15b2541
waibaBikash <kavrelibikash@gmail.com>
2026-02-18 22:13:31 +1100

    save change

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -6,7 +6,8 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
-        <footer className="flex flex-wrap justify-center md:justify-between overflow-hidden gap-10 md:gap-20 mt-40 py-6 px-6 md:px-16 lg:px-24 xl:px-32 text-[13px] text-gray-500">
+        <footer 
+        className="flex flex-wrap justify-center md:justify-between overflow-hidden gap-10 md:gap-20 mt-40 py-6 px-6 md:px-16 lg:px-24 xl:px-32 text-[13px] text-gray-500">
             <motion.div className="flex flex-wrap items-start gap-10 md:gap-35"
                 initial={{ x: -150, opacity: 0 }}
                 whileInView={{ x: 0, opacity: 1 }}
@@ -31,7 +32,8 @@ export default function Footer() {
                     </div>
                 ))}
             </motion.div>
-            <motion.div className="flex flex-col max-md:items-center max-md:text-center gap-2 items-end"
+            <motion.div
+             className="flex flex-col max-md:items-center max-md:text-center gap-2 items-end"
                 initial={{ x: 150, opacity: 0 }}
                 whileInView={{ x: 0, opacity: 1 }}
                 viewport={{ once: true }}
@@ -52,7 +54,9 @@ export default function Footer() {
                         <YoutubeIcon className="size-6 hover:text-pink-500" />
                     </a>
                 </div>
-                <p className="mt-3 text-center">&copy; {new Date().getFullYear()} <a href="https://prebuiltui.com?utm_source=pixels">Thumblyfy</a></p>
+                <p
+                  className="mt-3 text-center">&copy; {new Date().getFullYear()}
+                   <a href="https://prebuiltui.com?utm_source=pixels">Thumblyfy</a></p>
             </motion.div>
         </footer>
     );

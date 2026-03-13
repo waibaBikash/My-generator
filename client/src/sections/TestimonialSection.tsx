36f89f0adbc73bdaa6b68675e206482a316cf706
waibaBikash <kavrelibikash@gmail.com>
2026-03-13 22:18:03 +1100

    style: improve formatting of TestimonialSection component for better readability

--- client/src/sections/TestimonialSection.tsx
+++ client/src/sections/TestimonialSection.tsx
@@ -6,10 +6,16 @@ import Marquee from "react-fast-marquee";
 
 export default function TestimonialSection() {
     return (
-        <div id="testimonials" className="px-4 md:px-16 lg:px-24 xl:px-32">
-            <SectionTitle text1="Testimonials" text2="Loved by creators" text3="See how our AI thumbnails are helping channels explode their views." />
+        <div id="testimonials
+        " className="px-4 md:px-16 lg:px-24 xl:px-32">
+            <SectionTitle text1="Testimonials"
+             text2="Loved by creators" 
+             text3="See how our AI thumbnails are helping channels explode their views." />
 
-            <Marquee className="max-w-5xl mx-auto mt-11" gradient={true} speed={25} gradientColor="#000">
+            <Marquee 
+            className="max-w-5xl mx-auto mt-11" 
+            gradient={true} speed={25} 
+            gradientColor="#000">
                 <div className="flex items-center justify-center py-5 overflow-hidden">
                     {[...testimonialsData, ...testimonialsData].map((testimonial: ITestimonial, index: number) => (
                         <TestimonialCard key={index} index={index} testimonial={testimonial} />

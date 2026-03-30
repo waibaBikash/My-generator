bd95a7399e3ddc97565b2f593bc1f27e3e191516
waibaBikash <kavrelibikash@gmail.com>
2026-03-30 22:45:03 +1100

    style: improve formatting of CTASection component for better readability

--- client/src/sections/CTASection.tsx
+++ client/src/sections/CTASection.tsx
@@ -6,7 +6,8 @@ export default function CTASection() {
 
     const navigate = useNavigate()
     return (
-        <motion.div className="max-w-5xl py-16 mt-40 md:pl-20 md:w-full max-md:mx-4 md:mx-auto flex flex-col md:flex-row max-md:gap-6 items-center justify-between text-left bg-linear-to-b from-pink-900 to-pink-950 rounded-2xl p-6 text-white"
+        <motion.div 
+        className="max-w-5xl py-16 mt-40 md:pl-20 md:w-full max-md:mx-4 md:mx-auto flex flex-col md:flex-row max-md:gap-6 items-center justify-between text-left bg-linear-to-b from-pink-900 to-pink-950 rounded-2xl p-6 text-white"
             initial={{ y: 150, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
             viewport={{ once: true }}
